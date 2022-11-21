823d2af5ac787482dea71f16640065a37b53fd96
Halkano <kudezx@gmail.com>
2022-11-22 00:22:44 +0300

    done

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -3,13 +3,17 @@ class CheesesController < ApplicationController
   # GET /cheeses
   def index
     cheeses = Cheese.all
-    render json: cheeses
+    render json: cheeses, only: [:id,:price,:is_best_seller], methods: [:summary] 
   end
 
   # GET /cheeses/:id
   def show
     cheese = Cheese.find_by(id: params[:id])
-    render json: cheese
+    if cheese
+      render json: cheese
+    else
+      render json: {error: "Cheese not Found!!!"}, status: :not_found
+    end
   end
 
 end
